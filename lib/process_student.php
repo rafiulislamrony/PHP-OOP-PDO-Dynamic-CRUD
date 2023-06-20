c0797d054489abebcda1f0e6c307b0f6e0cf38b1
Rafiul Islam Rony <121638251+rafiulislamrony@users.noreply.github.com>
2023-06-20 23:19:07 +0600

    Insert class, process_student.php, Session class,

--- lib/process_student.php
+++ lib/process_student.php
@@ -1,8 +1,10 @@
 <?php
 
 include 'Database.php';
+include 'Session.php';
+Session::init();
 $db = new Database();
-$table = "tbl_student";
+$table = "tbl_student"; 
 
 if(isset($_REQUEST['action']) && !empty($_REQUEST['action'])){
     if($_REQUEST['action']== 'add'){
@@ -12,14 +14,17 @@ if(isset($_REQUEST['action']) && !empty($_REQUEST['action'])){
             'phone' => $_POST['phone'],
         );
         $insert = $db->insert($table, $studentData);
+
+        if($insert){
+            $msg = "Data Inserted Successfully.";
+        }else{
+            $msg = "Data not Inserted.";
+        } 
+        Session::set('msg', $msg);
+        $home_url = '../index.php';
+        header('Location: '. $home_url);
     }
 }
-
-
-
-?>
-
-<?php
 
 
 ?>
